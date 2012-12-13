a5d10787be12f36873e48cc4431ee38e82e28143
Manfred Moser <manfred@simpligility.com>
2012-12-13 14:56:44 -0800

    copying build output into root folder so that users dont have to build the slides

--- presentation/generate.sh
+++ presentation/generate.sh
@@ -9,4 +9,4 @@ cp -R images target/
 asciidoc --backend slidy --attribute theme=sonatype-slides --attribute stylesdir=`pwd`/stylesheets --attribute scriptsdir=`pwd`/javascripts -o target/nexus-presentation.html src/nexus-presentation.asciidoc
 rsync -avz src/images target
 
-#a2x -v -fpdf -darticle --dblatex-opts=" -P latex.output.revhistory=0" -D target src/nexus-presentation.asciidoc
+cp target/nexus-presentation.html ..
