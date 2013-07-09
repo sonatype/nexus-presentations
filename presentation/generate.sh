bea7c9aaced64eeb78a65d754a33650a8a9cd414
Manfred Moser <manfred@simpligility.com>
2013-07-09 12:07:06 -0700

    fixed stylesheet so it works again as stylesheet rather than theme

--- presentation/generate.sh
+++ presentation/generate.sh
@@ -4,9 +4,8 @@ set -e
 
 mkdir -p target
 cp -R stylesheets target/
-cp -R javascripts target/
 cp -R images target/
-asciidoc --backend slidy --attribute theme=sonatype-slides --attribute stylesdir=`pwd`/stylesheets --attribute scriptsdir=`pwd`/javascripts -o target/nexus-presentation.html src/nexus-presentation.asciidoc
+asciidoc --backend slidy --attribute stylesheet=`pwd`/stylesheets/sonatype-slides.css -o target/nexus-presentation.html src/nexus-presentation.asciidoc
 rsync -avz src/images target
 
 cp target/nexus-presentation.html ..
