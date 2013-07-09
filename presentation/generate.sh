2fda645a61703abba94ca55db2b9d12b4233c249
Manfred Moser <manfred@simpligility.com>
2013-07-09 12:10:22 -0700

    dramatically simplified script, still works

--- presentation/generate.sh
+++ presentation/generate.sh
@@ -2,10 +2,4 @@
 
 set -e 
 
-mkdir -p target
-cp -R stylesheets target/
-cp -R images target/
-asciidoc --backend slidy --attribute stylesheet=`pwd`/stylesheets/sonatype-slides.css -o target/nexus-presentation.html src/nexus-presentation.asciidoc
-rsync -avz src/images target
-
-cp target/nexus-presentation.html ..
+asciidoc --backend slidy --attribute stylesheet=`pwd`/stylesheets/sonatype-slides.css -o ../nexus-presentation.html src/nexus-presentation.asciidoc
