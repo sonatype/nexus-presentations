cb8df19549812e58e71582ca761e952334334589
Manfred Moser <manfred@simpligility.com>
2014-09-12 17:21:46 -0700

    setup 2 new presos for user group and Java One presentation and started creation of content

--- presentations/generate.sh
+++ presentations/generate.sh
@@ -2,8 +2,11 @@
 
 set -e 
 
-asciidoc -v -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/sonatype-slides.css -o ../nexus-introduction.html nexus-introduction.asciidoc
+asciidoc -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/sonatype-slides.css -o ../nexus-introduction.html nexus-introduction.asciidoc
+echo "Built nexus-introduction.html"
 
-#asciidoc -v -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/sonatype-slides.css -o ../nexus-oss-advantages.html nexus-oss-advantages.asciidoc
+asciidoc -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/sonatype-slides.css -o ../nexus-oss-advantages.html nexus-oss-advantages.asciidoc
+echo "Built nexus-oss-advantages.html"
 
-#asciidoc -v -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/sonatype-slides.css -o ../nexus-for-component-management.html nexus-for-component-management.asciidoc
+asciidoc -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/sonatype-slides.css -o ../nexus-for-component-management.html nexus-for-component-management.asciidoc
+echo "Built nexus-for-component-management.html"
