ab7d15c9702adb8a21554fa9423de29a159995b1
Manfred Moser <manfred@simpligility.com>
2015-07-08 12:27:52 -0700

    updated script

--- generate.sh
+++ generate.sh
@@ -7,14 +7,17 @@ set -u
 
 
 function render {
+    echo "--------------------"
+    echo -e "Started Processing $1\n"
     asciidoc -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/src/sonatype-slides.css -o $1.html src/$1.asciidoc
-    echo "Built $1.html"
+    echo "  Built $1.html"
     
     a2x -k -fpdf --dblatex-opts="-P toc.section.depth=1 -P latex.output.revhistory=0 -P doc.publisher.show=0" -D `pwd` src/$1.asciidoc
-    echo "Built $1.pdf"
+    echo "  Built $1.pdf"
+    echo -e "Completed Processing $1\n"
 }
 
-#render nexus-introduction
-#render nexus-oss-advantages
-#render nexus-for-component-management
+render nexus-introduction
+render nexus-oss-advantages
+render nexus-for-component-management
 render component-software-supply-chain
